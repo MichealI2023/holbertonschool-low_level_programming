86fc52f1ead6df06116e25e4145a3387b7734158
MichealI2023 <micheal.imoe@gmail.com>
2023-05-30 13:41:07 -0700

    m

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main- print_alphabet - prints alphabet
+ * @k - computed varible
+ *
+ * Return: Always 0 (Success)
+ */
+
+void (print_alphabet(void))
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+
+_putchar('\n');
+
+return (0);
+}
