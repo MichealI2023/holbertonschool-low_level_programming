c8303650ddd02a5620df9707f31b72b8f0a51b2f
MichealI2023 <micheal.imoe@gmail.com>
2023-05-30 15:48:20 -0700

    m

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - will print  + if positive and - if negative
+ * @n: if 1 is +, if 0 is 0, if -1 is -
+ * Return: 1 prints + if n>0,0 prints 0 if n=0, -1 prints - if n<0
+ */
+#include <stdio.h>
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+");
+return (1); /* Positive number */
+}
+else if (n == 0)
+{
+printf("0");
+return (0); /* Zero */
+}
+else
+{
+printf("-");
+return (-1); /* Negative number */
+}
+}
