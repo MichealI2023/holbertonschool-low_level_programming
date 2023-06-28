025283aa3343168512ec95d73b46e5bcb9408f91
MichealI2023 <micheal.imoe@gmail.com>
2023-06-28 11:14:54 -0700

    m

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  void _puts_recursion - proto
+ *  @s: intger
+ *  Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s);
+
+{
+	for(char*s = "Puts with recursion")
+	printf (*s /n)
+
+	return (0);
+}
+		
