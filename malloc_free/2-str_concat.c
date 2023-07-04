fa73de4fc144490d5d91dade36c02ddfa56e4a85
MichealI2023 <micheal.imoe@gmail.com>
2023-07-04 16:24:46 -0700

    m

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
-	while(s2[ci] != '\0')
+	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
